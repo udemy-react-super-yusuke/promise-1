88edfa88e59bd78ae136f561a8efd04b8422ad67
udemy-react-super-yusuke <38956215+udemy-react-super-yusuke@users.noreply.github.com>
2018-05-04 01:31:15 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# promise-1
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "react-dom";
+
+import axios from "axios";
+
+class App extends React.Component {
+  render() {
+    return <button onClick={this.handleDo}>Do</button>;
+  }
+
+  handleDo = () => {
+    const url = "https://developer.mozilla.org/en-US/docs/Web/HTML";
+
+    axios.get(url).then(res => {
+      console.log(res.data);
+      document.body.innerHTML = res.data;
+    });
+  };
+}
+
+render(<App />, document.getElementById("root"));
